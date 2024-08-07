68bec92f1f601bac0bd27d7f384124b0618317e3
axelwarn2 <722shock722@mail.ru>
2024-08-07 10:55:24 +0500

    13721:Изменит 'arResult' на '$arParams'

--- src/templates/main/footer.php
+++ src/templates/main/footer.php
@@ -1,31 +1,31 @@
 <footer class="footer">
     <div class="footer__content">
         <div class="footer__block footer__block_brand">
-            <h2 class="footer__title"><?= $arResultFooter["foot_title"] ?></h2>
-            <p class="footer__subtitle"><?= $arResultFooter["foot_subtitle"] ?></p>
+            <h2 class="footer__title"><?= $arParams["foot_title"] ?></h2>
+            <p class="footer__subtitle"><?= $arParams["foot_subtitle"] ?></p>
         </div>
         <div class="footer__block footer__block_menu">
-            <p class="footer__header"><?= $arResultFooter["menuname"] ?></p>
-            <?php foreach ($arResultFooter["menu"] as $footerMenuItem) : ?>
+            <p class="footer__header"><?= $arParams["menuname"] ?></p>
+            <?php foreach ($arParams["menu"] as $footerMenuItem) : ?>
                 <p class="footer__menu-item"><?= $footerMenuItem["heading"] ?></p>
             <?php endforeach; ?>
         </div>
         <div class="footer__block footer__block_contacts">
-            <p class="footer__header"><?= $arResultFooter["contactname"] ?></p>
+            <p class="footer__header"><?= $arParams["contactname"] ?></p>
             <div class="footer__contacts">
-                <?php foreach ($arResultFooter["contact"] as $footerContactItem) : ?>
+                <?php foreach ($arParams["contact"] as $footerContactItem) : ?>
                     <p class="footer__contact-item"><?= $footerContactItem["heading"] ?></p>
                 <?php endforeach; ?>
             </div>
         </div>
         <div class="footer__block footer__block_auth">
             <a href="../../../../authentication.php"><button class="footer__auth-button">
-                <?= $arResultFooter["foot_button"] ?></button>
+                <?= $arParams["foot_button"] ?></button>
             </a>
         </div>
     </div>
     <div class="footer__bottom">
-        <?php foreach ($arResultFooter["bottom"] as $footerBottomItem) : ?>
+        <?php foreach ($arParams["bottom"] as $footerBottomItem) : ?>
             <p class="footer__rights"><?= $footerBottomItem["heading"] ?></p>
         <?php endforeach; ?>
     </div>
--- src/templates/main/header.php
+++ src/templates/main/header.php
@@ -1,15 +1,15 @@
 <header class="header">
     <div class="header__logo-inner">
-        <span class="header__logo"><?= $arResultHeader["logo"] ?></span>
-        <p class="header__logo-desc"><?= $arResultHeader["desk"] ?></p>
+        <span class="header__logo"><?= $arParams["logo"] ?></span>
+        <p class="header__logo-desc"><?= $arParams["desk"] ?></p>
     </div>
     <nav class="header__navbar">
-        <?php foreach ($arResultHeader["navbar"] as $navbarItem) : ?>
+        <?php foreach ($arParams["navbar"] as $navbarItem) : ?>
             <a href="<?= $navbarItem["link"] ?>" class="header__item"><?= $navbarItem["heading"] ?></a>
         <?php endforeach; ?>
     </nav>
     <div class="header__content">
-        <p class="content__number"><?= $arResultHeader["number"] ?></p>
-        <button class="button header__button header__button_secondary"><?= $arResultHeader["header_button"] ?></button>
+        <p class="content__number"><?= $arParams["number"] ?></p>
+        <button class="button header__button header__button_secondary"><?= $arParams["header_button"] ?></button>
     </div>
 </header>
